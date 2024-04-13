a4a3bc1f33eeae400b8d178ecd8d6bca4c62ed96
Hanzdev <huynhngocanhittt@gmail.com>
2024-04-13 15:21:06 +0700

    Init Code

---
+++ src/server.js
@@ -0,0 +1,17 @@
+// import express from 'express'
+// const express = require('express')
+import express from 'express'
+
+const app = express()
+
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World</h1>')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`hanzdev at ${hostname}:${port}/`)
+})
+
