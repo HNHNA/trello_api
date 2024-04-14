fb2f561845e66416015495158e9b0a8bc855a3f2
Hanzdev <huynhngocanhittt@gmail.com>
2024-04-15 01:21:06 +0700

    Config Express Router & Test API

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
 
@@ -13,10 +14,7 @@ const START_SERVER = () => {
   const hostname = 'localhost'
   const port = 8017
 
-  app.get('/', async (req, res) => {
-    // console.log(await GET_DB().listCollections().toArray())
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`3. Hello ${env.AUTHOR}, Backend Server is running at ${ hostname }:${ port }/`)
