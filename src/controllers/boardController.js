e46d4e7e223c3ca487aa7bd1178591b00cd5de08
Hanzdev <huynhngocanhittt@gmail.com>
2024-04-15 08:13:06 +0700

    Error Handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,14 +1,24 @@
 import { StatusCodes } from 'http-status-codes'
-
+import ApiError from '~/utils/ApiErrors'
 
 const createNew = async ( req, res, next ) => {
   try {
-    console.log('req.body:', req.body)
+  //   console.log('req.body:', req.body)
+  //   console.log('req.query:', req.query)
+  //   console.log('req.params:', req.params)
+  //   console.log('req.files:', req.files)
+  //   console.log('req.cookies:', req.cookies)
+  //   console.log('req.jwtDecoded:', req.jwtDecoded)
+
+
+    // throw new ApiError(StatusCodes.BAD_GATEWAY, 'hanzdev error')
+    // Có kết quả thì trả về phía Clients
     res.status(StatusCodes.CREATED).json({ message: ' post: from Controller: API create new board' })
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: new Error(error).message
-    })
+    next(error)
+    // res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    //   errors: new Error(error).message
+    // })
   }
 }
 
